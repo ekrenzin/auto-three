84db43fbd4b17741b38b798f34d3a8b985575114
Ean Krenzin-Blank <ean@eankrenzin.com>
2023-04-29 12:56:19 -0700

    basic loading

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -1,24 +1,41 @@
 <script lang="ts">
   import { LangChain } from './components/langchain/index'
   import { onMount } from 'svelte'
-  
-  let text = ''
+
+  let text: string = ''
+  let langchain: LangChain
+  let textOutput: string = ''
+  let loading: boolean = false
 
   onMount(async () => {
     const api_key = await window.electron.ipcRenderer.invoke('open-ai-key')
     LangChain.init(api_key)
+    langchain = LangChain.getInstance()
   })
 
   async function submitText() {
-    const model = LangChain.getModel()
-    console.log(model)
+    loading = true
+    try {
+      textOutput = await langchain.complete(text)
+    } catch (e) {
+      console.log(e)
+    }
+    loading = false
   }
 </script>
 
 <main class="container">
-  <h1>Auto Three</h1>
-  <textarea bind:value={text} />
-  <button on:click={submitText}> Submit </button>
+  {#if !loading}
+    <h1>Auto Three</h1>
+    <textarea bind:value={text} />
+    <button on:click={submitText}> Submit </button>
+    <div>
+      <h2>Output</h2>
+      <p>{textOutput}</p>
+    </div>
+  {:else}
+    <h1>Loading...</h1>
+  {/if}
 </main>
 
 <style>
